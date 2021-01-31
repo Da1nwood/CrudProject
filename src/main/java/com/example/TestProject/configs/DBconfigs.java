341e5caf426dc21636f3c717696f17698db76c5e
nikita.petrov <nikita.petrov@ab-soft.net>
2021-01-31 22:37:46 +0200

    Code style refactoring

---
+++ src/main/java/com/example/TestProject/configs/DBconfigs.java
@@ -0,0 +1,17 @@
+package com.example.TestProject.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+public class DBconfigs {
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
